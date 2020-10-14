005bac05399881f0bacbb85c8591b8713947b6b3
David5627 <binginaya708@gmail.com>
2020-10-14 10:50:38 +0300

    modify js

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $("#piza1").hover(function(){
+        $(".overlay1").toggle();
+    });
+    $("#piza2").hover(function(){
+        $(".overlay2").toggle();
+    });
+    $("#piza3").hover(function(){
+        $(".overlay3").toggle();
+    });
+    $("#piza4").hover(function(){
+        $(".overlay4").toggle();
+    });
+    $("#piza5").hover(function(){
+        $(".overlay5").toggle();
+    });
+    $("#piza6").hover(function(){
+        $(".overlay6").toggle();
+    });
+    $("#piza7").hover(function(){
+        $(".overlay7").toggle();
+    });
+    $("#piza8").hover(function(){
+        $(".overlay8").toggle();
+    });
+})
